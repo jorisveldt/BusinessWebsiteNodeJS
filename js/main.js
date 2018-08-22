3435a75582b1c989e4f7f766abe1d5ce9875142a
Joris Veldt <Joris@MacBook-Pro.local>
2018-08-22 17:53:29 +0200

    I finished the sign-up page and started with the program page. Here i wanted to add some jQuery to it but now it just mess up the navbar, so I need to continue from that part tomorrow. Jens will finish about page. And will start looking at the program page.

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//Hover function for program
+
+$( "li" ).hover(
+    function() {
+      $( this ).append( $( "<span> ***</span>" ) );
+    }, function() {
+      $( this ).find( "span:last" ).remove();
+    }
+  );
+   
+  $( "li.fade" ).hover(function() {
+    $( this ).fadeOut( 100 );
+    $( this ).fadeIn( 500 );
+  });
